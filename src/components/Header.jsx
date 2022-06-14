57e4352f30338342f211089c9b762c8ff06cc662
khafidhkayyis <khafidhkayyis@gmail.com>
2022-06-14 15:08:46 +0700

    add/header

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import { faBed, faCar, faPlane } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "../assets/scss/header.scss";
+export const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__container">
+        <div className="header__list">
+          <div className="header__list-item">
+            <FontAwesomeIcon icon={faBed} />
+            <span>stay</span>
+          </div>
+          <div className="header__list-item">
+            <FontAwesomeIcon icon={faPlane} />
+            <span>flight</span>
+          </div>
+          <div className="header__list-item">
+            <FontAwesomeIcon icon={faCar} />
+            <span>car rentals</span>
+          </div>
+          <div className="header__list-item">
+            <FontAwesomeIcon icon={faBed} />
+            <span>attractions</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import "../assets/scss/navbar.scss";
+const Navbar = () => {
+  return (
+    <header>
+      <nav className="navbar">
+        <div className="navbar__container">
+          <h1 className="navbar__logo">BokingApp</h1>
+          <div className="navbar__items">
+            <button className="navbar__register">Register</button>
+            <button className="navbar__login">Login</button>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
