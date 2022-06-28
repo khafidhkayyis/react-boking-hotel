9e5cbf786dc47d1b40ff8150ce5086317dbf0efa
khafidhkayyis <khafidhkayyis@gmail.com>
2022-06-28 12:19:39 +0700

    add/header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,20 +5,20 @@ export const Header = () => {
   return (
     <div className="header">
       <div className="header__container">
-        <div className="header__list">
-          <div className="header__list-item">
+        <div className="header__list ">
+          <div className="header__list__item ">
             <FontAwesomeIcon icon={faBed} />
             <span>stay</span>
           </div>
-          <div className="header__list-item">
+          <div className="header__list__item active">
             <FontAwesomeIcon icon={faPlane} />
             <span>flight</span>
           </div>
-          <div className="header__list-item">
+          <div className="header__list__item">
             <FontAwesomeIcon icon={faCar} />
             <span>car rentals</span>
           </div>
-          <div className="header__list-item">
+          <div className="header__list__item">
             <FontAwesomeIcon icon={faBed} />
             <span>attractions</span>
           </div>
